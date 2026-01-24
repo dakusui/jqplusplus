ef5cc004f74dc1f8ff50d441daddb0324bf98ec0
Hiroshi Ukai <dakusui@gmail.com>
2026-01-25 08:58:26 +0900

    Fix a broken test

--- cmd/jqplusplus/main_test.go
+++ cmd/jqplusplus/main_test.go
@@ -36,3 +36,27 @@ func TestLoadAndResolveInheritances_SingleExtendsForJqFile(t *testing.T) {
 		t.Errorf("expected %v, got %v", expected, result)
 	}
 }
+func TestLoadAndResolveInheritances_TagRef(t *testing.T) {
+	dir := t.TempDir()
+	child := testutil.WriteTempJSON(t, dir, "child.json",
+		`{
+  "thetag": "Hello, mytag",
+  "k0": {
+    "k1": "eval:string:reftag(\"thetag\")"
+  }
+}`)
+	result, err := processNodeEntryKey((internal.NewNodeEntryKey(filepath.Dir(child), filepath.Base(child))))
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	expected, _ := json.MarshalIndent(
+		map[string]any{
+			"thetag": "Hello, mytag",
+			"k0": map[string]any{
+				"k1": "Hello, mytag"}},
+		"", "  ")
+	if !reflect.DeepEqual(result, string(expected)) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
